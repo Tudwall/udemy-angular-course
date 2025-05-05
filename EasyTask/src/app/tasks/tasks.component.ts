90b94cb4d2a865968829b66529e9c5e232846498
Pierre <pierre.laffaille@outlook.fr>
2025-05-05 10:37:38 +0200

    add new task dialog and closing logic

--- EasyTask/src/app/tasks/tasks.component.ts
+++ EasyTask/src/app/tasks/tasks.component.ts
@@ -1,16 +1,20 @@
 import { Component, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
 @Input({required: true}) name?: string;
 @Input({required: true}) userId!: string;
+
+isAddingTask = false;
+
 tasks = [
   {
     id: 't1',
@@ -43,5 +47,13 @@ get selectedUserTasks() {
 
 onCompleteTask(id:string) {
   this.tasks = this.tasks.filter((task) => task.id !== id)
+}
+
+onStartAddTask() {
+  this.isAddingTask = true;
+}
+
+onCancelAddTask() {
+  this.isAddingTask = false;
 }
 }
