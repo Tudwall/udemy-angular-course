88f7dc92e8c0267b55a01160d25140c1e0233d61
Pierre <pierre.laffaille@outlook.fr>
2025-05-09 16:58:59 +0200

    use service

--- InvestmentCalculator/src/app/investment-results/investment-results.component.ts
+++ InvestmentCalculator/src/app/investment-results/investment-results.component.ts
@@ -1,6 +1,6 @@
-import { Component, input } from '@angular/core';
-import { ResultsData } from '../results-data.model';
+import { Component, inject} from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
@@ -10,5 +10,9 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  results = input<ResultsData[]>()
+  private investmentService = inject(InvestmentService)
+
+  get results() {
+    return this.investmentService.resultData
+  }
 }
