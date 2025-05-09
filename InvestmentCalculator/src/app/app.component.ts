2396ef6f8ca81cb50db68332855215266c3bfb1a
Pierre <pierre.laffaille@outlook.fr>
2025-05-09 16:38:46 +0200

    use signals and reset form

--- InvestmentCalculator/src/app/app.component.ts
+++ InvestmentCalculator/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import type { InvestmentInput } from './investment-input.model';
@@ -12,7 +12,7 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  resultsData?: ResultsData[]
+  resultsData=signal<ResultsData[] | undefined>(undefined)
   onCalculateInvestmentResults(data: InvestmentInput) {
   const {initialInvestment, duration, expectedReturn, annualInvestment} = data
   const annualData = [];
@@ -34,6 +34,6 @@ export class AppComponent {
     });
   }
 
-this.resultsData = annualData;
+this.resultsData.set(annualData)
 }
 }
