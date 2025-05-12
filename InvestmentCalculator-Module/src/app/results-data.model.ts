328edbb866a7b8905f549e82ebc0857950734977
Pierre Laffaille <pierre.laffaille@outlook.fr>
2025-05-12 10:23:44 +0200

    finish debugging chapter

--- InvestmentCalculator-Module/src/app/results-data.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ResultsData {
-      year: number,
-      interest: number,
-      valueEndOfYear: number,
-      annualInvestment: number,
-      totalInterest: number,
-      totalAmountInvested: number
-    }
